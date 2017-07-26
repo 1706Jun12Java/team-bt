28bd7b687f405fe18b877ed645f50d9274e6f2ac
alan <alancao101@gmail.com>
2017-07-26 15:14:44 -0400

    test

--- src/main/java/com/bt/main/Driver.java
+++ src/main/java/com/bt/main/Driver.java
@@ -14,23 +14,6 @@ public class Driver {
 //        init();
 //        testURDao();
     	
-    	System.out.println("Test");
-
-    	String db = System.getenv("$(env.DB_URL)");
-    	
-    	System.out.println(db);
-    	String db2 = System.getProperty("${env.DB_URL}");
-    	
-    	System.out.println(db2);
-    	
-//    	String testing1 = System.getenv("$(env.DATABASE)");
-//    	
-//    	System.out.println(testing1);
-//    	
-//    	String testing2 = System.getenv("$(env.testdatabase)");
-//    	
-//    	System.out.println(testing1);
-    	
     }
     static void init() {
 
