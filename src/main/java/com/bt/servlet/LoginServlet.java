3d1cc3804e003cdbd95d744dd04025fcf2fa5076
iRzLi <irzli1995@gmail.com>
2017-07-26 16:43:25 -0400

    servlets

--- src/main/java/com/bt/servlet/LoginServlet.java
+++ src/main/java/com/bt/servlet/LoginServlet.java
@@ -1,8 +1,18 @@
 package com.bt.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bt.dao.UserRoleDao;
+import com.bt.dao.UserRoleDaoImpl;
+import com.bt.domain.User;
+import com.bt.domain.UserRole;
 
 public class LoginServlet extends HttpServlet{
 
@@ -13,11 +23,26 @@ public class LoginServlet extends HttpServlet{
 	public LoginServlet() {
 		// TODO Auto-generated constructor stub
 	}
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		/*
+		try {
+			resp.sendRedirect("index.html");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		PrintWriter pw = resp.getWriter();
+		//HttpSession session = req.getSession();
+		resp.setContentType("text/html");
+		UserRoleDao urDao = new UserRoleDaoImpl();
+		for(UserRole ur: urDao.getUserRoles()){
+			pw.println(ur.toString());
+		}
+		
 		
 	}
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-		
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		System.out.println("bye");
 	}
 
 }
