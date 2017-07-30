e1beb3c003e1d467f6a9f66c042edf11ca9db532
lizhou <alancao101@gmail.com>
2017-07-30 18:46:11 -0400

    te

--- src/main/java/com/bt/controllers/TestController.java
+++ src/main/java/com/bt/controllers/TestController.java
@@ -2,12 +2,12 @@ package com.bt.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/test")
 public class TestController {
-	@RequestMapping(value="/1")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String test(){
-		return "html/test.html";
+		return "html/index.html";
 	}
 }
