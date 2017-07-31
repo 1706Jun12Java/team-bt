f87cfe3facfd31d7a5f774afc0fdf086e7556c09
alan <alancao101@gmail.com>
2017-07-31 09:45:57 -0400

    Server JSP/CSS/JS

--- src/main/java/com/bt/controllers/TestController.java
+++ src/main/java/com/bt/controllers/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="*", method=RequestMethod.GET)
 	public String test(){
-		return "html/index.html";
+		return "index";
 	}
 }
