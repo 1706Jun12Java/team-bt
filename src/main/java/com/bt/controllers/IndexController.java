3563e0c7702af5070c39507ebdf15780f3da5914
alan <alancao101@gmail.com>
2017-08-02 16:48:19 -0400

    test push

--- src/main/java/com/bt/controllers/IndexController.java
+++ src/main/java/com/bt/controllers/IndexController.java
@@ -18,7 +18,7 @@ import java.io.PrintWriter;
 public class IndexController {
     @RequestMapping(value="*", method= RequestMethod.GET)
     public String test(){
-        return "static/index.html";
+        return "index";
     }
     @RequestMapping(value="/test", method=RequestMethod.GET)
     public void getInfo(HttpServletRequest req, HttpServletResponse resp){
