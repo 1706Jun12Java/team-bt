054231efc145d2022062ea5a04733d8b7ff60dc9
lizhou <alancao101@gmail.com>
2017-08-05 12:10:35 -0400

    test

--- src/main/java/com/bt/controllers/UserController.java
+++ src/main/java/com/bt/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.bt.controllers;
 import com.bt.domain.User;
 import com.google.gson.Gson;
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,7 +14,7 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(value="/login",method=RequestMethod.POST)
-    public User loginUser(@RequestBody String user){
+    public ResponseEntity loginUser(@RequestBody String user){
         Gson gson = new Gson();
 
         User userInfo = gson.fromJson(user, User.class);
@@ -22,7 +24,20 @@ public class UserController {
 
         userInfo.setPassword(null);
 
-        return userInfo;
+        return new ResponseEntity<>(userInfo, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/register",method=RequestMethod.POST)
+    public ResponseEntity registerUser(@RequestBody String user){
+        Gson gson = new Gson();
+        User userInfo = gson.fromJson(user, User.class);
+
+
+
+        userInfo.setPassword(null);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
