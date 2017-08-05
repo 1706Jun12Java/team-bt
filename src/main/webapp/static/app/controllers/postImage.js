9c747b5310c2a9928bdcc350cafa4ab287cfc11d
iRzLi <irzli1995@gmail.com>
2017-08-05 18:21:16 -0400

    update

--- src/main/webapp/static/app/controllers/postImage.js
+++ src/main/webapp/static/app/controllers/postImage.js
@@ -5,10 +5,10 @@
         .module('app')
         .controller('PostImageCtrl', ['$scope', '$http', '$location', 'userService', function($scope, $http, $location, userService){
             $scope.newImage = function() {
-                $scope.postImage.image = getBase64($scope.postImage.image);
                 $http.post('/postImage', $scope.postImage)
                     .then(function(response) {
                         console.log("sucess");
+                        console.log($scope.postImage);
                         userService.setInfo(response.data);
                         $location.path('/');
                     }, function (error) {
