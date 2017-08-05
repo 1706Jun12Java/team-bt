7cf19f40a8dcd80cdec9a139868c0ecfe4615f97
iRzLi <irzli1995@gmail.com>
2017-08-05 17:27:44 -0400

    update

--- src/main/webapp/static/app/controllers/login.js
+++ src/main/webapp/static/app/controllers/login.js
@@ -3,7 +3,7 @@
 
     angular
         .module('app')
-        .controller('LoginCtrl', ['$scope', '$http', '$location','userService', function($scope, $http, $location, userService){
+        .controller('LoginCtrl', ['$scope', '$http', '$location', 'userService', function($scope, $http, $location, userService){
             $scope.loginUser = function() {
                 $http.post('/login', $scope.user)
                     .then(function(response) {
